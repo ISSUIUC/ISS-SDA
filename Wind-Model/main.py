433a9d6f2455a04b2397352b93a1fe17564c2eb3
mpkarpov-ui <mkarpov2@illinois.edu>
2024-03-31 01:34:26 -0500

    stormglass wrapper

--- Wind-Model/main.py
+++ Wind-Model/main.py
@@ -1,28 +1,41 @@
 # ADD STUFF HERE!
 import API_KEY
-import requests
-import json
+import util.stormglass
+import matplotlib.pyplot as plt
 
-params = "waterTemperature,wavePeriod,waveDirection,waveHeight,windWaveDirection,windWaveHeight,windWavePeriod,swellPeriod,secondarySwellPeriod,swellDirection,secondarySwellDirection,swellHeight,secondarySwellHeight,windSpeed,windSpeed20m,windSpeed30m,windSpeed40m,windSpeed50m,windSpeed80m,windSpeed100m,windSpeed1000hpa,windSpeed800hpa,windSpeed500hpa,windSpeed200hpa,windDirection,windDirection20m,windDirection30m,windDirection40m,windDirection50m,windDirection80m,windDirection100m,windDirection1000hpa,windDirection800hpa,windDirection500hpa,windDirection200hpa,airTemperature,airTemperature80m,airTemperature100m,airTemperature1000hpa,airTemperature800hpa,airTemperature500hpa,airTemperature200hpa,precipitation,gust,cloudCover,humidity,pressure,visibility,currentSpeed,currentDirection,iceCover,snowDepth,seaLevel"
-response = requests.get(
-  'https://api.stormglass.io/v2/weather/point',
-  params={
-    'lat': 40.112283,
-    'lng': -88.226541,
-    'params': params,
-  },
-  headers={
-    'Authorization': API_KEY.API_KEY
-  }
-)
-
-json_data = response.json()
-print(json_data)
+# CONFIGURATION
+latitude = 40.112283
+longitude = -88.226541
 
 
-with open("./out.json", "a") as f:
-    f.write(str(json_data))
 
+
+
+
+#################
+
+dataloader = util.stormglass.StormGlass(API_KEY.API_KEY)
+dataloader.load_file("./dl_out.json")
+# dataloader.load_data(latitude, longitude)
+# dataloader.dump_file("./dl_out.json")
+
+
+df: util.stormglass.StormGlassData = dataloader.get_dataframe()
+
+ax = plt.figure().add_subplot(projection='3d')
+
+for ind, row in enumerate(df[0:23]):
+    a, s, _ = row.get_wind_gradient_raw()
+    ax.plot(a, s, zs=ind)
+
+ax.set_xlabel("Altitude (m)")
+ax.set_ylabel("Wind speed (m/s)")
+ax.set_zlabel("Hour")
+
+plt.show()
+
+      
+    
 
 
 
